6448c3011f288ee783e122acf2acf601d2203f48
Keenan <kvenuti@gmail.com>
2020-11-08 21:47:47 -0800

    Add files via upload

---
+++ configs/config.py
@@ -0,0 +1,39 @@
+#servo channel mappings under my current PCA cont
+SERVO_CHANNELS = {
+'THUMB':0,
+'INDEX':1,
+'MIDDLE':2,
+'RING':3,
+'PINKY':4,
+'WRIST_ROTATE':5,
+'WRIST_BEND':6,
+'ELBOW':7,
+'SHOULDER_ROTATE':8,
+'SHOULDER_BEND':9
+}
+#servo ranges TODO
+SERVO_RANGES = {
+'THUMB':(0,0),
+'INDEX':(0,0),
+'MIDDLE':(0,0),
+'RING':(0,0),
+'PINKY':(0,0),
+'WRIST_ROTATE':(0,0),
+'WRIST_BEND':(0,0),
+'ELBOW':(0,0),
+'SHOULDER_ROTATE':(0,0),
+'SHOULDER_BEND':(0,0)
+}
+#default starter pos value, for now .5 through range
+DEFAULT_STARTS = {
+'THUMB':0.5,
+'INDEX':0.5,
+'MIDDLE':0.5,
+'RING':0.5,
+'PINKY':0.5,
+'WRIST_ROTATE':0.5,
+'WRIST_BEND':0.5,
+'ELBOW':0.5,
+'SHOULDER_ROTATE':0.5,
+'SHOULDER_BEND':0.5
+}
